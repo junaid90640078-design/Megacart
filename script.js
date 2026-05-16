7dd0bd17f0154b342b5f938595edec5e61aff7ab
Junaid Rahaman <junaid90640078@gmail.com>
2026-05-17 02:06:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const buttons = document.querySelectorAll(".cart-btn");
 
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    alert("Product Added To Cart");
+
+  });
+
+});
